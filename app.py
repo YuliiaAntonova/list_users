aedb77dc7e8b2b7db88312c467709e016a5933b6
Yuliia Antonova <yuliiaantonova@Yuliias-MacBook-Pro.local>
2022-07-26 13:21:04 +0200

    db settings

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
