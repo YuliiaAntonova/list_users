c10d30fb469caaf012711ddac4d1ea8583aa7b24
Yuliia Antonova <yuliiaantonova@Yuliias-MacBook-Pro.local>
2022-07-26 17:59:48 +0200

    Database setup

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yaxkxzjufavypv:a204c1f0f3d81bd1ce13e27d7d17c3e08b0b208661d9a98b6b9fd53bc8cc7904@ec2-52-49-120-150.eu-west-1.compute.amazonaws.com:5432/dabjf6hd6mfu3a'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
